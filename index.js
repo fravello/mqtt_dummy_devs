a1da5b2a7e18d6a4eb3e5da1365c6e1d3c9fb7f0
Fabian Ravello Valenzuela <fabian.ravello@gmail.com>
2019-11-10 17:42:05 -0300

    .

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on('connect', function () {
     setInterval(
         function(){
 
-            client.publish(config.mqtt.topics, '{"datetime":"'+ new Date().toLocaleString() +'"}, "sensor":"tmp", "value":"'+ Math.floor(Math.random() * (41 - 20 + 1) + 20) +'"');
+            client.publish(config.mqtt.topics, '{"datetime":"'+ new Date().toLocaleString() +'"}, "sensor":"tmp", "value":"'+ Math.floor(Math.random() * (41 - 20 + 1) + 20) +'"}');
             client.publish(config.mqtt.topics, '{"datetime":"'+ new Date().toLocaleString() +'"}, "sensor":"hum", "value":"'+ Math.floor(Math.random() * (80 - 50 + 1) + 50) +'"}');
             client.publish(config.mqtt.topics, '{"datetime":"'+ new Date().toLocaleString() +'"}, sensor":"aper", "value":"'+ Math.floor(Math.random() * 2) +'"}');
             client.publish(config.mqtt.topics, '{datetime":"'+ new Date().toLocaleString() +'"}, sensor":"gas", "value":"'+ Math.floor(Math.random() * 2) +'"}');
